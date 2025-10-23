56c44a9250c6f7b6068121afad7b85fb62d23c76
d1yordev <diorkh07@gmail.com>
2025-10-23 11:01:50 +0800

    I submit my day:1 Homework

---
+++ Homeworks/about_me.py
@@ -0,0 +1,15 @@
+# day ! pythonLearning Homework:
+"""■ Homework Task
+Create a Python file called about_me.py. Write a program that asks for: - Your name - Your age -
+Your favorite AI tool Then print a personalized message like this:
+Enter your name: Diyor Enter your age: 20 Enter your favorite AI tool: TuronAI
+--------------------------------- Hello Diyor! You are 20 years old and love TuronAI. Welcome to the AI
+learning journey!"""
+
+name = input("What is your name? ")
+age = input("How old Are you? ")
+AI = input("Favorite AI Tool's name?")
+
+result = f"Hello {name}, You are {age} years old and love {AI}.Welcome to the AI Learning Journey!"
+
+print(result)
